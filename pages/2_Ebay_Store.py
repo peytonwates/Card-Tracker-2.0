2147bb6d9d5f7534fa4b932b54fcff252bf757be
Peyton Wates <peyton.wates@gmail.com>
2026-06-23 14:05:28 -0500

    soemthing

--- pages/2_Ebay_Store.py
+++ pages/2_Ebay_Store.py
@@ -2412,7 +2412,3 @@ with tab_audit:
     if order_payload:
         with st.expander("Raw Fulfillment API order response", expanded=False):
             st.json(order_payload)
-
-
-
-# notes for me
